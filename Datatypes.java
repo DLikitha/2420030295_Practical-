88c6dad6d8e421d59edfd3fa77e3109aa083ba87
DLikitha <2420030295@klh.edu.in>
2025-08-05 10:33:05 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+package myproject;
+
+public class Datatypes {
+   public static void main(String args[]) {
+	   int a=10;
+	   float b=20.6F;
+	   long d=9920331191l;
+	   long g= 2420030295l;
+	   char c ='A';
+	   String string = "Kukatpally,Hyderbad";
+	   boolean e = true;
+	   String name="Likitha";
+	   //System.out.println("Integer"+a);
+	   //System.out.println(b);
+	   //System.out.println(c);
+	   System.out.println(name);
+	   //System.out.println(e);
+	   System.out.println(d);
+	   System.out.println(g);
+	   System.out.println(string);
+   }
+}
